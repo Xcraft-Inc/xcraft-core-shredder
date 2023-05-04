49389c4dc1665c76f7317bd3c243a3f45ec3ce2b
Mathieu Schroeter <mathieu@schroetersa.ch>
2023-05-04 23:58:04 +0200

    Cosmetics

--- test/shredder.spec.js
+++ test/shredder.spec.js
@@ -7,147 +7,149 @@ const bimBam = {bim: 'bam'};
 const bimBamBoum = [];
 const grosMinet = {gros: 'minet'};
 
-describe('shredder', function () {
-  it('set', function () {
-    const s = new Shredder(bimBam);
-    const n = s.set('titi', grosMinet);
+describe('xcraft.shredder', function () {
+  describe('usage', function () {
+    it('set', function () {
+      const s = new Shredder(bimBam);
+      const n = s.set('titi', grosMinet);
 
-    expect(n.toJS()).to.be.eql(
-      Object.assign({}, bimBam, {
-        titi: grosMinet,
-      })
-    );
+      expect(n.toJS()).to.be.eql(
+        Object.assign({}, bimBam, {
+          titi: grosMinet,
+        })
+      );
 
-    expect(s.toJS()).to.be.eql(bimBam);
-  });
-
-  it('set an empty path', function () {
-    const s = new Shredder(bimBam);
-
-    const n = s.set('', grosMinet);
-
-    expect(n.toJS()).to.be.eql({gros: 'minet'});
-    expect(s.toJS()).to.be.eql(bimBam);
-  });
-
-  it('set a simple path with a bracket', function () {
-    const s = new Shredder(bimBam);
-
-    const n = s.set('boom[1]', grosMinet);
-
-    expect(n.toJS()).to.be.eql({
-      bim: 'bam',
-      boom: [undefined, {gros: 'minet'}],
-    });
-    expect(s.toJS()).to.be.eql(bimBam);
-  });
-
-  it('set a cool path', function () {
-    const s = new Shredder(bimBam);
-
-    const n = s.set('blim.bla.boom[1].splif[3].splaf', grosMinet);
-
-    expect(n.toJS()).to.be.eql({
-      bim: 'bam',
-      blim: {
-        bla: {
-          boom: [
-            undefined,
-            {
-              splif: [
-                undefined,
-                undefined,
-                undefined,
-                {
-                  splaf: grosMinet,
-                },
-              ],
-            },
-          ],
-        },
-      },
+      expect(s.toJS()).to.be.eql(bimBam);
     });
 
-    expect(s.toJS()).to.be.eql(bimBam);
-  });
+    it('set an empty path', function () {
+      const s = new Shredder(bimBam);
 
-  it('del', function () {
-    const s = new Shredder(bimBam);
-    const n = s.del('bim');
+      const n = s.set('', grosMinet);
 
-    expect(n.toJS()).to.be.eql({});
-  });
+      expect(n.toJS()).to.be.eql({gros: 'minet'});
+      expect(s.toJS()).to.be.eql(bimBam);
+    });
 
-  it('find', function () {
-    // Return value (not immutable)
-    let s = new Shredder(['glace', 'chat', 'bob']);
-    let res = s.find((item) => item === 'chat');
-    expect(res).to.be.eql('chat');
+    it('set a simple path with a bracket', function () {
+      const s = new Shredder(bimBam);
 
-    // Return shreddder immutable
-    s = new Shredder(
-      fromJS([{value: 'glace'}, {value: 'chat'}, {value: 'bob'}])
-    );
-    res = s.find((item) => item.get('value') === 'chat');
-    expect(res).to.be.eql(s.get('1'));
+      const n = s.set('boom[1]', grosMinet);
 
-    // Return null
-    s = new Shredder(
-      fromJS([{value: 'glace'}, {value: 'chat'}, {value: 'bob'}])
-    );
-    res = s.find((item) => item.get('value') === 'test');
-    expect(res).to.be.eql(null);
-  });
-});
-
-describe('shredder (performance)', function () {
-  it(`test set/get (×2'000'000)`, function () {
-    this.timeout(10000);
-
-    let l = new Shredder(bimBamBoum);
-
-    for (let i = 1; i <= 3; ++i) {
-      let path;
-      console.time(`test ${i}`);
-      for (let i = 0; i < 2_000_000; i++) {
-        if (i % 100 === 0) {
-          path = `_${process.hrtime.bigint()}._${process.hrtime.bigint()}`;
-        }
-        l = l.set(path, i);
-        l.get(path);
-      }
-      console.timeEnd(`test ${i}`);
-    }
-  });
-
-  it(`test map vs mapShredder (×2'000'000)`, function () {
-    this.timeout(10000);
-
-    let l = new Shredder(bimBamBoum);
-
-    for (let i = 0; i < 2_000_000; i++) {
-      l = l.push('', i);
-    }
-
-    for (let i = 1; i <= 3; i++) {
-      console.time(`test ${i} map`);
-      l.map((item) => item);
-      console.timeEnd(`test ${i} map`);
-    }
-
-    l = new Shredder(bimBamBoum);
-
-    for (let i = 0; i < 2_000_000; i++) {
-      l = l.push('', i);
-    }
-
-    for (let i = 1; i <= 3; i++) {
-      console.time(`test ${i} mapShredder`);
-      l.map((item, ...args) => {
-        item = new Shredder(item);
-        return (item, ...args) => item;
+      expect(n.toJS()).to.be.eql({
+        bim: 'bam',
+        boom: [undefined, {gros: 'minet'}],
       });
-      console.timeEnd(`test ${i} mapShredder`);
-    }
+      expect(s.toJS()).to.be.eql(bimBam);
+    });
+
+    it('set a cool path', function () {
+      const s = new Shredder(bimBam);
+
+      const n = s.set('blim.bla.boom[1].splif[3].splaf', grosMinet);
+
+      expect(n.toJS()).to.be.eql({
+        bim: 'bam',
+        blim: {
+          bla: {
+            boom: [
+              undefined,
+              {
+                splif: [
+                  undefined,
+                  undefined,
+                  undefined,
+                  {
+                    splaf: grosMinet,
+                  },
+                ],
+              },
+            ],
+          },
+        },
+      });
+
+      expect(s.toJS()).to.be.eql(bimBam);
+    });
+
+    it('del', function () {
+      const s = new Shredder(bimBam);
+      const n = s.del('bim');
+
+      expect(n.toJS()).to.be.eql({});
+    });
+
+    it('find', function () {
+      // Return value (not immutable)
+      let s = new Shredder(['glace', 'chat', 'bob']);
+      let res = s.find((item) => item === 'chat');
+      expect(res).to.be.eql('chat');
+
+      // Return shreddder immutable
+      s = new Shredder(
+        fromJS([{value: 'glace'}, {value: 'chat'}, {value: 'bob'}])
+      );
+      res = s.find((item) => item.get('value') === 'chat');
+      expect(res).to.be.eql(s.get('1'));
+
+      // Return null
+      s = new Shredder(
+        fromJS([{value: 'glace'}, {value: 'chat'}, {value: 'bob'}])
+      );
+      res = s.find((item) => item.get('value') === 'test');
+      expect(res).to.be.eql(null);
+    });
+  });
+
+  describe('performance', function () {
+    it(`test set/get (×2'000'000)`, function () {
+      this.timeout(10000);
+
+      let l = new Shredder(bimBamBoum);
+
+      for (let i = 1; i <= 3; ++i) {
+        let path;
+        console.time(`test ${i}`);
+        for (let i = 0; i < 2_000_000; i++) {
+          if (i % 100 === 0) {
+            path = `_${process.hrtime.bigint()}._${process.hrtime.bigint()}`;
+          }
+          l = l.set(path, i);
+          l.get(path);
+        }
+        console.timeEnd(`test ${i}`);
+      }
+    });
+
+    it(`test map vs mapShredder (×2'000'000)`, function () {
+      this.timeout(10000);
+
+      let l = new Shredder(bimBamBoum);
+
+      for (let i = 0; i < 2_000_000; i++) {
+        l = l.push('', i);
+      }
+
+      for (let i = 1; i <= 3; i++) {
+        console.time(`test ${i} map`);
+        l.map((item) => item);
+        console.timeEnd(`test ${i} map`);
+      }
+
+      l = new Shredder(bimBamBoum);
+
+      for (let i = 0; i < 2_000_000; i++) {
+        l = l.push('', i);
+      }
+
+      for (let i = 1; i <= 3; i++) {
+        console.time(`test ${i} mapShredder`);
+        l.map((item, ...args) => {
+          item = new Shredder(item);
+          return (item, ...args) => item;
+        });
+        console.timeEnd(`test ${i} mapShredder`);
+      }
+    });
   });
 });
